c9d8b0ae7098bcd9bfa5b31774f7afaaf6e1f4a2
Md Ali Armaghan <aliarmaghan78@gmail.com>
2025-04-04 22:53:11 +0530

    Add drive utilities for Google Drive integration

---
+++ drive_utils.py
@@ -0,0 +1,38 @@
+# drive_utils.py
+
+import os
+import io
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def authenticate_drive():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'client_secrets.json', SCOPES
+    )
+    creds = flow.run_local_server(port=0)
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+def list_folders(service):
+    results = service.files().list(
+        q="mimeType='application/vnd.google-apps.folder' and trashed=false",
+        fields="files(id, name)"
+    ).execute()
+    return results.get('files', [])
+
+def list_pdfs_in_folder(service, folder_id):
+    query = f"'{folder_id}' in parents and mimeType='application/pdf'"
+    results = service.files().list(q=query, fields="files(id, name)").execute()
+    return results.get('files', [])
+
+def download_pdf(service, file_id, filename, download_path='resumes/'):
+    os.makedirs(download_path, exist_ok=True)
+    request = service.files().get_media(fileId=file_id)
+    fh = io.FileIO(os.path.join(download_path, filename), 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while not done:
+        _, done = downloader.next_chunk()
