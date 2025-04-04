fecaf889125c09efb0bc744b0af2b9b5b2d4cec5
Md Ali Armaghan <aliarmaghan78@gmail.com>
2025-04-05 00:59:05 +0530

    Add webbrowser import to drive_utils.py for handling URL redirection

--- drive_utils.py
+++ drive_utils.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 import streamlit as st
 from google.oauth2.service_account import Credentials
+import webbrowser
 
 # SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
