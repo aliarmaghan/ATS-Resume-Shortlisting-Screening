825289c471856edd161e6944ec4e1f12020952ca
Md Ali Armaghan <aliarmaghan78@gmail.com>
2025-04-05 08:28:35 +0530

    Refactor authenticate_drive to load credentials from Streamlit secrets and create OAuth flow using InstalledAppFlow

--- drive_utils.py
+++ drive_utils.py
@@ -6,16 +6,46 @@ from googleapiclient.http import MediaIoBaseDownload
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
+# SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+# def authenticate_drive():
+#     flow = InstalledAppFlow.from_client_secrets_file(
+#         'client_secrets.json', SCOPES
+#     )
+
+#     creds = flow.run_local_server(port=0)
+#     service = build('drive', 'v3', credentials=creds)
+#     return service
+
+import streamlit as st
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 def authenticate_drive():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        'client_secrets.json', SCOPES
-    )
+    # Load credentials from Streamlit secrets
+    credentials_info = {
+        "installed": {
+            "client_id": st.secrets["installed"]["client_id"],
+            "project_id": st.secrets["installed"]["project_id"],
+            "auth_uri": st.secrets["installed"]["auth_uri"],
+            "token_uri": st.secrets["installed"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["installed"]["auth_provider_x509_cert_url"],
+            "client_secret": st.secrets["installed"]["client_secret"],
+            "redirect_uris": st.secrets["installed"]["redirect_uris"]
+        }
+    }
 
+    # Create OAuth flow
+    flow = InstalledAppFlow.from_client_config(credentials_info, SCOPES)
     creds = flow.run_local_server(port=0)
+
+    # Build the service
     service = build('drive', 'v3', credentials=creds)
+    
     return service
+
 
 # # def authenticate_drive():
 # #     """Authenticate with Google Drive using credentials stored in Streamlit secrets"""
